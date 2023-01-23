4072b0a896507aaab68accd5602eb5fb87776615
Tetiana <shahdi007@gmail.com>
2023-01-23 15:47:27 -0500

    'change names'

--- src/app/base/mock-todo-items.ts
+++ src/app/base/mock-todo-items.ts
@@ -3,13 +3,13 @@ import {ToDoItem} from './toDoItem'
 
 export const ITEMS:ToDoItem[] = [
     {id: 1, name: 'John', isComplite: false},
-    {id: 2, name: 'Jhon2', isComplite: false},
-    {id: 3, name: 'Jhon3', isComplite: false},
-    {id: 4, name: 'Jhon4', isComplite: false},
-    {id: 5, name: 'Jhon5', isComplite: false},
-    {id: 6, name: 'Jhon6', isComplite: false},
-    {id: 7, name: 'Jhon7', isComplite: false},
-    {id: 8, name: 'Jhon8', isComplite: false},
-    {id: 9, name: 'Jhon9', isComplite: false},
-    {id: 10, name: 'Jhon10', isComplite: false}
+    {id: 2, name: 'John2', isComplite: false},
+    {id: 3, name: 'John3', isComplite: false},
+    {id: 4, name: 'John4', isComplite: false},
+    {id: 5, name: 'John5', isComplite: false},
+    {id: 6, name: 'John6', isComplite: false},
+    {id: 7, name: 'John7', isComplite: false},
+    {id: 8, name: 'John8', isComplite: false},
+    {id: 9, name: 'John9', isComplite: false},
+    {id: 10, name: 'John10', isComplite: false}
 ]
